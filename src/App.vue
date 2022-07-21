a88b098758e96cc3ab9deff14f4780bb37ab8e25
binohub <yeebino001@gmail.com>
2022-07-21 15:31:36 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app"> 
+    <headerSite />
+    <mainSite />
+  </div>
+</template>
+
+<script>
+import headerSite from './components/headerSite.vue'
+import mainSite from './components/mainSite.vue'
+
+export default {
+  name: 'App',
+  components: {
+    headerSite,
+    mainSite,
+  }
+  data: function(){
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap.scss";
+
+</style>
